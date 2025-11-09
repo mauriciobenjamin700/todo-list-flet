f7c4dfba8bc3ecfb7ba0db35199e8f59b41f6208
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2025-11-09 14:29:59 -0300

    ref: change flet default components for custom components in register page

--- src/views/register.py
+++ src/views/register.py
@@ -1,6 +1,13 @@
 import flet as ft
+from pydantic import ValidationError
 
-from src.components import Button, TextButton, Input
+from src.components import (
+    Button,
+    TextButton,
+    Input,
+    CenterContainer,
+    VerticalContainer
+)
 from src.schemas import UserCreateSchema
 from src.controllers import UserController
 
@@ -17,22 +24,26 @@ def main(page: ft.Page):
     message = ft.Text("")
 
     def submit(e):
-        data = UserCreateSchema(
-            name=name.value,
-            email=email.value,
-            password=password.value
-        )
         try:
+            data = UserCreateSchema(
+                name=name.value,
+                email=email.value,
+                password=password.value
+            )
             controller.add(data)
             message.value = "Conta criada com sucesso!"
             page.go("/login")
+        except ValidationError:
+            message.value = "Parece que tem algo de errado com seus dados"
+            message.color = ft.Colors.RED_300
         except Exception as ex:
             message.value = str(ex)
+            message.color = ft.Colors.RED_300
         message.update()
 
     page.add(
-        ft.Container(
-            ft.Column(
+        CenterContainer(
+            VerticalContainer(
                 [
                     name, email, password,
                     Button(text="Criar conta", on_click=submit),
@@ -42,12 +53,7 @@ def main(page: ft.Page):
                     ),
                     message
                 ],
-                alignment=ft.MainAxisAlignment.CENTER,
-                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             ),
-            expand=True,
-            bgcolor=ft.Colors.BLACK54,
-            alignment=ft.alignment.center,
         ),
 
 
